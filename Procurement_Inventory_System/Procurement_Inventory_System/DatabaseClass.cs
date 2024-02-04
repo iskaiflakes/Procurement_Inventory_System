b78eab0befda7ab12c2e316c6fb06fbb7d6f0a18
iskaiflakes <cole.cabansag@gmail.com>
2024-02-04 18:17:16 +0800

    DatabaseClass (Done - To be tested)
    - created method for insert, delete and update
    - created method for retrieving method only (for select sql statements)

--- Procurement_Inventory_System/Procurement_Inventory_System/DatabaseClass.cs
+++ Procurement_Inventory_System/Procurement_Inventory_System/DatabaseClass.cs
@@ -9,17 +9,37 @@ namespace Procurement_Inventory_System
 {
     public class DatabaseClass
     {
-        SqlConnection sqlConnection;
-        string connectionString;
-        SqlCommand sqlCom;
+        private SqlConnection sqlConnection;
+        private string connectionString;
+        private SqlCommand sqlCom;
+        private SqlDataReader sqlReader;
 
-        public void ConnectDatabase(string dataSource)
+        public void ConnectDatabase(string dataSource)  // call this first every time we perform CRUD
         {
             connectionString = "Data Source="+dataSource+"\\SQLEXPRESS;Initial Catalog=Procurement_Inventory_System;Integrated Security=True";
+            sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = connectionString;
             sqlConnection.Open();
         }
 
-        public void CloseConnection()
+        public int insDelUp(string sqlStatement)    // method for insert, delete and update
+        {
+            sqlCom = new SqlCommand(sqlStatement, sqlConnection);   
+            int x = sqlCom.ExecuteNonQuery();   
+            CloseConnection();
+
+            return x;   // returns a value of -1 if the CRUD operation is unsuccessful (used as an indicator to check if there is a duplication)
+        }
+
+        public SqlDataReader GetRecord(string sqlStatement) // method used to retrieve data
+        {
+            sqlCom = new SqlCommand(sqlStatement, sqlConnection);
+            sqlReader = sqlCom.ExecuteReader();
+
+            return sqlReader;   // returns one row from the table
+        }
+
+        public void CloseConnection()   // call this to close connection
         {
             sqlConnection.Close();
         }
