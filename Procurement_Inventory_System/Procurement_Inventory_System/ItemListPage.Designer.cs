3309ba00d7bf390fe14903553a48b252f08dd87d
ThatOneLeaf <106152167+ThatOneLeaf@users.noreply.github.com>
2024-05-13 20:53:28 +0800

    added filter for item list table

--- Procurement_Inventory_System/Procurement_Inventory_System/ItemListPage.Designer.cs
+++ Procurement_Inventory_System/Procurement_Inventory_System/ItemListPage.Designer.cs
@@ -28,15 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dashboard = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.SelectStatus = new System.Windows.Forms.ComboBox();
-            this.SelectDate = new System.Windows.Forms.ComboBox();
+            this.SelectSupplier = new System.Windows.Forms.ComboBox();
             this.updatesplybtn = new System.Windows.Forms.Button();
             this.addnewsplybtn = new System.Windows.Forms.Button();
             this.searchUser = new System.Windows.Forms.TextBox();
@@ -81,17 +81,19 @@
             this.SelectStatus.Size = new System.Drawing.Size(99, 28);
             this.SelectStatus.TabIndex = 27;
             this.SelectStatus.Text = "  (Status)";
+            this.SelectStatus.SelectedIndexChanged += new System.EventHandler(this.SelectStatus_SelectedIndexChanged);
             // 
-            // SelectDate
+            // SelectSupplier
             // 
-            this.SelectDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.SelectDate.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SelectDate.FormattingEnabled = true;
-            this.SelectDate.Location = new System.Drawing.Point(562, 115);
-            this.SelectDate.Name = "SelectDate";
-            this.SelectDate.Size = new System.Drawing.Size(133, 28);
-            this.SelectDate.TabIndex = 25;
-            this.SelectDate.Text = "  (Supplier)";
+            this.SelectSupplier.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SelectSupplier.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SelectSupplier.FormattingEnabled = true;
+            this.SelectSupplier.Location = new System.Drawing.Point(562, 115);
+            this.SelectSupplier.Name = "SelectSupplier";
+            this.SelectSupplier.Size = new System.Drawing.Size(133, 28);
+            this.SelectSupplier.TabIndex = 25;
+            this.SelectSupplier.Text = "  (Supplier)";
+            this.SelectSupplier.SelectedIndexChanged += new System.EventHandler(this.SelectDate_SelectedIndexChanged);
             // 
             // updatesplybtn
             // 
@@ -142,51 +144,51 @@
             // 
             // dataGridView1
             // 
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle1.NullValue = null;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Maroon;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White;
-            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.NullValue = null;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.Maroon;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.White;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle6;
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Maroon;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Maroon;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.Maroon;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.Maroon;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.Maroon;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.Maroon;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle8;
             this.dataGridView1.GridColor = System.Drawing.Color.DimGray;
             this.dataGridView1.Location = new System.Drawing.Point(32, 158);
             this.dataGridView1.Name = "dataGridView1";
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.Maroon;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.Color.Maroon;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
             this.dataGridView1.RowHeadersWidth = 51;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.Maroon;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.White;
-            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle10.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.Color.Maroon;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.Color.White;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle10;
             this.dataGridView1.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.Gray;
             this.dataGridView1.Size = new System.Drawing.Size(663, 444);
             this.dataGridView1.TabIndex = 105;
@@ -201,7 +203,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.SelectStatus);
-            this.Controls.Add(this.SelectDate);
+            this.Controls.Add(this.SelectSupplier);
             this.Controls.Add(this.updatesplybtn);
             this.Controls.Add(this.addnewsplybtn);
             this.Controls.Add(this.searchUser);
@@ -220,7 +222,7 @@
         private System.Windows.Forms.Label dashboard;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ComboBox SelectStatus;
-        private System.Windows.Forms.ComboBox SelectDate;
+        private System.Windows.Forms.ComboBox SelectSupplier;
         private System.Windows.Forms.Button updatesplybtn;
         private System.Windows.Forms.Button addnewsplybtn;
         private System.Windows.Forms.TextBox searchUser;
--- Procurement_Inventory_System/Procurement_Inventory_System/ItemListPage.cs
+++ Procurement_Inventory_System/Procurement_Inventory_System/ItemListPage.cs
@@ -47,16 +47,44 @@ namespace Procurement_Inventory_System
         }
         public void LoadItemList()
         {
+            // Create a new DataTable with the desired schema
             DataTable item_table = new DataTable();
+            item_table.Columns.Add("ITEM ID", typeof(string));
+            item_table.Columns.Add("ITEM NAME", typeof(string));
+            item_table.Columns.Add("DESCRIPTION", typeof(string));
+            item_table.Columns.Add("SECTION ID", typeof(string));
+            item_table.Columns.Add("SUPPLIER", typeof(string));
+            item_table.Columns.Add("ACTIVE", typeof(string)); // Make ACTIVE column a string
+
+            // Populate the new DataTable with data from the database
             DatabaseClass db = new DatabaseClass();
             db.ConnectDatabase();
             string department = CurrentUserDetails.DepartmentId;
             string section = CurrentUserDetails.DepartmentSection;
-            string query = $"SELECT il.item_id AS 'ITEM ID', il.item_name AS 'ITEM NAME', il.item_description AS 'DESCRIPTION', il.section_id AS 'SECTION ID', su.supplier_name AS SUPPLIER, il.active AS 'ACTIVE' FROM Item_List il JOIN Supplier su ON il.supplier_id=su.supplier_id WHERE il.department_id='{department}' AND il.section_id='{section}' ORDER BY il.item_name";
+            string query = $"SELECT il.item_id, il.item_name, il.item_description, il.section_id, su.supplier_name, il.active FROM Item_List il JOIN Supplier su ON il.supplier_id=su.supplier_id WHERE il.department_id='{department}' AND il.section_id='{section}' ORDER BY il.item_name";
             SqlDataAdapter da = db.GetMultipleRecords(query);
-            da.Fill(item_table);
-            dataGridView1.DataSource = item_table;
+            DataTable temp_table = new DataTable();
+            da.Fill(temp_table);
             db.CloseConnection();
+
+            // Convert ACTIVE column values from 0/1 to Inactive/Active and import rows
+            foreach (DataRow row in temp_table.Rows)
+            {
+                DataRow newRow = item_table.NewRow();
+                newRow["ITEM ID"] = row["item_id"].ToString();
+                newRow["ITEM NAME"] = row["item_name"].ToString();
+                newRow["DESCRIPTION"] = row["item_description"].ToString();
+                newRow["SECTION ID"] = row["section_id"].ToString();
+                newRow["SUPPLIER"] = row["supplier_name"].ToString();
+                newRow["ACTIVE"] = row["active"].ToString() == "1" ? "Active" : "Inactive";
+                item_table.Rows.Add(newRow);
+            }
+
+            dataGridView1.DataSource = item_table;
+
+            // Populate filter dropdowns
+            PopulateStatus();
+            PopulateSupplier();
         }
 
 
@@ -85,7 +113,74 @@ namespace Procurement_Inventory_System
         {
             (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = string.Format("([Item ID] LIKE '%{0}%' OR [Item Name] LIKE '%{0}%')", searchUser.Text);
         }
+
+        private void SelectStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FilterData();
+        }
+
+        private void SelectDate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FilterData();
+        }
+        private void FilterData()
+        {
+            DataTable dt = (dataGridView1.DataSource as DataTable);
+
+            if (dt != null)
+            {
+                string statusFilter = SelectStatus.SelectedIndex > 0 ? SelectStatus.SelectedItem.ToString() : null;
+                string supplierFilter = SelectSupplier.SelectedIndex > 0 ? SelectSupplier.SelectedItem.ToString() : null;
+
+                StringBuilder filter = new StringBuilder();
+
+                if (SelectStatus.SelectedIndex > 0)
+                {
+                    filter.Append($"[ACTIVE] = '{statusFilter}'");
+                }
+
+                if (SelectSupplier.SelectedIndex > 0)
+                {
+                    if (filter.Length > 0)
+                    {
+                        filter.Append(" AND ");
+                    }
+                    filter.Append($"SUPPLIER = '{supplierFilter}'");
+                }
+
+                dt.DefaultView.RowFilter = filter.ToString();
+            }
+        }
+
+        public void PopulateStatus()
+        {
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            var distinctValues = dt.AsEnumerable()
+                                   .Select(row => row.Field<string>("ACTIVE"))
+                                   .Distinct()
+                                   .ToList();
+
+            distinctValues.Insert(0, "(Status)"); // Add placeholder
+
+            SelectStatus.DataSource = distinctValues;
+            SelectStatus.SelectedIndex = 0; // Ensure no default selection
+        }
+
+        public void PopulateSupplier()
+        {
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            var distinctValues = dt.AsEnumerable()
+                                   .Select(row => row.Field<string>("SUPPLIER"))
+                                   .Distinct()
+                                   .ToList();
+
+            distinctValues.Insert(0, "(Supplier)"); // Add placeholder
+
+            SelectSupplier.DataSource = distinctValues;
+            SelectSupplier.SelectedIndex = 0; // Ensure no default selection
+        }
     }
+}
     public static class ItemListValues //itemId, itemName, itemDescription, section, supplier, active
     {
         public static string ItemID { get; set; }
@@ -95,4 +190,4 @@ namespace Procurement_Inventory_System
         public static string ItemSupplier { get; set; }
         public static string ItemActive { get; set; }
     }
-}
+
