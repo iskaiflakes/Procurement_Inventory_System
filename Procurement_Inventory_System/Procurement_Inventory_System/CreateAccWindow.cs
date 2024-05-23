1d6c035f6ad85f45fa93c059331f9aa61d40dda3
Nicole Cabansag <111856754+iskaiflakes@users.noreply.github.com>
2024-05-23 11:12:49 +0800

    FIXED DEFECT - Account Module: Verify Creating Account (UT), UT-013
    - only allowing creating user account within that branch if the currently logged in user account is not from MOF
    - added restrictions for other admins in each branch in creating user account (should only create user account within the branch)

--- Procurement_Inventory_System/Procurement_Inventory_System/CreateAccWindow.cs
+++ Procurement_Inventory_System/Procurement_Inventory_System/CreateAccWindow.cs
@@ -137,7 +137,18 @@ namespace Procurement_Inventory_System
             DatabaseClass db = new DatabaseClass();
             db.ConnectDatabase();
 
-            string query = "select BRANCH_NAME from BRANCH"; // select all department name
+
+            string query = "";
+
+            if (CurrentUserDetails.BranchId == "MOF")
+            {
+                query = "select BRANCH_NAME from BRANCH";   // select all branch name
+            }
+            else
+            {
+                query = $"select BRANCH_NAME from BRANCH WHERE BRANCH_ID='{CurrentUserDetails.BranchId}'"; // only allowing creating user account within that branch if the currently logged in user account is not from MOF
+            }
+
             SqlDataReader dr = db.GetRecord(query);
 
             // Clear existing items to avoid duplication if this method is called more than once
